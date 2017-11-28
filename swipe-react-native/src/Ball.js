d164d2221b7bc820f4249262dea1d7bac299a22c
Salman Rana <salman.r.rana@gmail.com>
2017-11-27 20:23:40 -0600

    added dummy deck component data

--- swipe-react-native/src/Ball.js
+++ swipe-react-native/src/Ball.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { View, Animated } from 'react-native';
+import { View, Animated, Text } from 'react-native';
 
 class Ball extends Component {
   componentWillMount() {
     this.position = new Animated.ValueXY(0, 0);
     Animated.spring(this.position, {
-      toValue: { x: 200, y: 700 }
+      toValue: { x: 200, y: 500 }
     }).start();
   }
 
@@ -13,6 +13,7 @@ class Ball extends Component {
     return (
       <Animated.View style={this.position.getLayout()}>
         <View style={styles.ball} />
+        <Text>YO YO YO</Text>
       </Animated.View>
     );
   }
