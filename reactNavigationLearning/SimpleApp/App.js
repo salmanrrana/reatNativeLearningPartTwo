0a36002c3e5c7cb2dc5be1e1638158d584541365
Salman Rana <salman.r.rana@gmail.com>
2018-01-06 19:39:34 -0600

    more learning

--- reactNavigationLearning/SimpleApp/App.js
+++ reactNavigationLearning/SimpleApp/App.js
@@ -4,23 +4,21 @@ import { StackNavigator, TabNavigator } from 'react-navigation';
 
 import HomeScreen from './Components/HomeScreen';
 import ChatScreen from './Components/ChatScreen';
-// import RecentChatsScreen from './Components/RecentChatsScreen';
-// import AllContactsScreen from './Components/AllContactsScreen';
+import RecentChatsScreen from './Components/RecentChatsScreen';
+import AllContactsScreen from './Components/AllContactsScreen';
 
-const SimpleApp = StackNavigator({
+// export const MainScreenNavigator = TabNavigator({
+//   Recent: { screen: RecentChatsScreen },
+//   All: { screen: AllContactsScreen },
+// });
+
+
+export const SimpleApp = StackNavigator({
   Home: {
     screen: HomeScreen,
-    navigationOptions: {
-      title: 'My Chats',
-    },
   },
   Chat: { screen: ChatScreen },
 });
-
-// const MainScreenNavigator = TabNavigator({
-//   Recent: { screen: SimpleApp },
-//   All: { screen: AllContactsScreen },
-// });
 
 
 export default class App extends React.Component {
